209991c0e8e1aeb750ab3ee1f7154bb14e25e470
Dolev <vomwuc@gmail.com>
2020-03-18 20:20:26 +0200

    Gray background added to project. Application window now scale according to user screen size.

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        final double APPLICATION_SCALE = 5/6.0;
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root,
+                Screen.getPrimary().getBounds().getWidth() * APPLICATION_SCALE ,
+                Screen.getPrimary().getBounds().getHeight() * APPLICATION_SCALE,
+                Color.grayRgb(25)));
         primaryStage.show();
     }
 
